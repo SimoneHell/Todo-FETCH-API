cbb949beef4b22f25e67a6492df6729bb61abea6
SimoneHell <simone.hall.speaks@gmail.com>
2022-08-12 16:30:12 +0000

    Finished project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,57 +1,90 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
-import { faPlus} from '@fortawesome/free-solid-svg-icons'
-
-
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
 
-  const handleSubmit = (e) => {
+  const createTodo =(e) =>{   
     e.preventDefault();
-
     const newTodo = {
       id: new Date().getTime(),
-      text: todo,
-      completed: false,
+      label: todo,
+      done: false,
     };
-    setTodos([...todos].concat(newTodo));
-    setTodo("");
+    editTodos(todos.concat(newTodo))
+  }
+
+  const editTodos =(list) => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/simonehell", {
+      method: "PUT",
+      body: JSON.stringify(list),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          loader() 
+        }
+      })
+      
+      .catch((error) => {
+        //error handling
+        console.log(error);
+      });
+  }
+
+  const loader = async () => {
+    await fetch("https://assets.breatheco.de/apis/fake/todos/user/simonehell")
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.log(error));
   };
+
+  useEffect(() => {
+    loader();
+  }, []);
+
   const deleteTodo = (id) => {
     const updatedTodos = [...todos].filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
+    editTodos(updatedTodos)
   };
- 
-  return (<div className="Container">
-    <div className="App">
-      <h1>Things we gotta DO!</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={(e) => setTodo(e.target.value)}
-          value={todo}
-        />
-        <button type="submit">
-        <FontAwesomeIcon icon={faPlus} />
-        </button>
-      </form>
-	  <div>
-      {todos.map((todo) => (
-        <div key={todo.id} className="text-center" >
-          <div className="List" style={{display:"flex"}}>
-            <div>{todo.text}</div>
-            <button onClick={() => deleteTodo(todo.id)}>
-            <FontAwesomeIcon icon={faTrashCan} />
-            </button>
-          </div>
+
+  return (
+    <div className="Container">
+      <div className="App">
+        <h1>Things we gotta DO!</h1>
+        <form onSubmit={createTodo}>
+          <input
+            type="text"
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+          />
+          <button type="submit">
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </form>
+        <div>
+          {todos.map((todo) => {
+            if (todo.id) {
+              return (
+                <div key={todo.id} className="text-center">
+                  <div className="list" style={{ display: "flex" }}>
+                    <div>{todo.label}</div>
+                    <button onClick={() => deleteTodo(todo.id)}>
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </button>
+                  </div>
+                </div>
+              );
+            }
+          })}
         </div>
-      ))}
-	  </div>
+      </div>
     </div>
-	</div>
   );
 };
 
